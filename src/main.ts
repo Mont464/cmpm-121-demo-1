4c7e76eac61f45c970421bad52a1b3043bc48f03
Mont778 <alejandro.montoreano@gmail.com>
2024-10-09 00:04:03 -0700

    Finished Step 4: Added autoIncrement function that uses requestAnimationFrame and performance.now() to calculate the partial count to be added every frame

--- src/main.ts
+++ src/main.ts
@@ -18,13 +18,23 @@ const click_report = document.createElement("div");
 click_report.innerHTML = `Flasks Brewed: ${click_count}`;
 app.append(click_report);
 
-const incrementCounter = function() {
-    click_count++;
-    click_report.innerHTML = `Flasks Brewed: ${click_count}`;
-}
-
-button.onclick = () => {
-  incrementCounter();
+const incrementCounter = function (n:number) {
+  click_count += n;
+  click_report.innerHTML = `Flasks Brewed: ${click_count}`;
 };
 
-const auto_click_interval = setInterval(incrementCounter, 1000);
+button.onclick = () => {
+  incrementCounter(1);
+};
+
+let previous_time = performance.now();
+let current_time = 0;
+const autoIncrement = function () {
+    current_time = performance.now();
+    incrementCounter((current_time - previous_time)/1000);
+    previous_time = current_time;
+    requestAnimationFrame(autoIncrement);
+}
+
+requestAnimationFrame(autoIncrement);
+
