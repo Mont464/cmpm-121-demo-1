4485bf7356135b282da22ca40adb7a65db2ac0f4
Mont778 <alejandro.montoreano@gmail.com>
2024-10-09 00:36:39 -0700

    Finished Step 5: Added auto growth with upgrade

--- src/main.ts
+++ src/main.ts
@@ -9,32 +9,51 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("button");
-button.innerHTML = "⚗️";
-app.append(button);
+const clicker_button = document.createElement("button");
+clicker_button.innerHTML = "⚗️BREW⚗️";
+app.append(clicker_button);
 
 let click_count: number = 0;
 const click_report = document.createElement("div");
 click_report.innerHTML = `Flasks Brewed: ${click_count}`;
 app.append(click_report);
 
-const incrementCounter = function (n:number) {
+const incrementCounter = function (n: number) {
   click_count += n;
-  click_report.innerHTML = `Flasks Brewed: ${click_count}`;
+  click_report.innerHTML = `Flasks Brewed: ${click_count.toFixed(3)}`;
 };
 
-button.onclick = () => {
+clicker_button.onclick = () => {
   incrementCounter(1);
 };
+
+let auto_growth: number = 0;
+const growth_upgrade_button = document.createElement("button");
+growth_upgrade_button.innerHTML = `🧪Increase Auto Distillation🧪\nCurrent Level: ${auto_growth}`;
+app.append(growth_upgrade_button);
+growth_upgrade_button.disabled = true;
+
+growth_upgrade_button.onclick = () => {
+  if (click_count >= 10) {
+    click_count -= 10;
+    auto_growth++;
+    growth_upgrade_button.innerHTML = `🧪Increase Auto Distillation🧪\nCurrent Level: ${auto_growth}`;
+  }
+};
+
 
 let previous_time = performance.now();
 let current_time = 0;
 const autoIncrement = function () {
-    current_time = performance.now();
-    incrementCounter((current_time - previous_time)/1000);
-    previous_time = current_time;
-    requestAnimationFrame(autoIncrement);
-}
+  current_time = performance.now();
+  incrementCounter(auto_growth * ((current_time - previous_time) / 1000));
+  previous_time = current_time;
+
+  if(click_count >= 10) {
+    growth_upgrade_button.disabled = false;
+  }
+
+  requestAnimationFrame(autoIncrement);
+};
 
 requestAnimationFrame(autoIncrement);
-
