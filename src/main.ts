17f959cf5a7b41ef9e534525dc266202da50e021
Mont778 <alejandro.montoreano@gmail.com>
2024-11-03 19:17:29 -0800

    Refactor and Remove Code Smells

--- src/main.ts
+++ src/main.ts
@@ -9,89 +9,125 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const clicker_button = document.createElement("button");
-clicker_button.style.backgroundColor = "#9733ff";
-clicker_button.style.width = "150px";
-clicker_button.style.height = "150px";
-clicker_button.style.borderRadius = "50%";
-clicker_button.innerHTML = "⚗️<br>⚗️BREW⚗️<br>⚗️";
-clicker_button.style.textAlign = "center";
-clicker_button.style.fontSize = "20px";
+const clickerButton = document.createElement("button");
+clickerButton.style.backgroundColor = "#9733ff";
+clickerButton.style.width = "150px";
+clickerButton.style.height = "150px";
+clickerButton.style.borderRadius = "50%";
+clickerButton.innerHTML = "⚗️<br>⚗️BREW⚗️<br>⚗️";
+clickerButton.style.textAlign = "center";
+clickerButton.style.fontSize = "20px";
 
-app.append(clicker_button);
+app.append(clickerButton);
 
-let click_count: number = 0;
-const click_report = document.createElement("div");
-click_report.innerHTML = `Flasks Brewed: ${click_count}`;
-app.append(click_report);
+let clickCount: number = 0;
+const clickReport = document.createElement("div");
+clickReport.innerHTML = `Flasks Brewed: ${clickCount}`;
+app.append(clickReport);
 
 const incrementCounter = function (n: number) {
-  click_count += n;
-  click_report.innerHTML = `Flasks Brewed: ${click_count.toFixed(3)}`;
+  clickCount += n;
+  clickReport.innerHTML = `Flasks Brewed: ${clickCount.toFixed(3)}`;
 };
 
-clicker_button.onclick = () => {
+clickerButton.onclick = () => {
   incrementCounter(1);
 };
 
-let auto_growth: number = 0;
+let autoGrowth: number = 0;
 
 interface Item {
   name: string;
   cost: number;
   rate: number;
-  up_count: number;
+  upCount: number;
   description: string;
 }
 
 const availableItems: Item[] = [
-  { name: "Goblin Gardener", cost: 10, rate: 0.1, up_count: 0, description: "Resourceful goblin that gathers plants for new potions" },
-  { name: "Golden Cauldron", cost: 50, rate: 0.5, up_count: 0, description: "More Cauldrons = More Potions" },
-  { name: "Goblin Wizard", cost: 100, rate: 2, up_count: 0, description: "Magial maniacs that can clone resources and potions" },
-  { name: "Mystic Catalyst", cost: 1000, rate: 50, up_count: 0, description: "The most valuable item in alchemy" },
-  { name: "Gonk, the Omnipotent Goblin", cost: 10000, rate: 150, up_count: 0, description: "Gonk is he. Gonk help" },
+  {
+    name: "Goblin Gardener",
+    cost: 10,
+    rate: 0.1,
+    upCount: 0,
+    description: "Resourceful goblin that gathers plants for new potions",
+  },
+  {
+    name: "Golden Cauldron",
+    cost: 50,
+    rate: 0.5,
+    upCount: 0,
+    description: "More Cauldrons = More Potions",
+  },
+  {
+    name: "Goblin Wizard",
+    cost: 100,
+    rate: 2,
+    upCount: 0,
+    description: "Magial maniacs that can clone resources and potions",
+  },
+  {
+    name: "Mystic Catalyst",
+    cost: 1000,
+    rate: 50,
+    upCount: 0,
+    description: "The most valuable item in alchemy",
+  },
+  {
+    name: "Gonk, the Omnipotent Goblin",
+    cost: 10000,
+    rate: 150,
+    upCount: 0,
+    description: "Gonk is he. Gonk help",
+  },
 ];
 
 const buttons: HTMLButtonElement[] = [];
+const buttonCostScalar: number = 1.15;
 for (let i = 0; i < availableItems.length; i++) {
   buttons[i] = document.createElement("button");
   buttons[i].innerHTML =
-    `${availableItems[i].cost.toFixed(2)} Flasks🧪: Get ${availableItems[i].name}<br>${availableItems[i].description}<br>Increases Auto Generation by ${availableItems[i].rate}<br>Current Level: ${availableItems[i].up_count}`;
+    `${availableItems[i].cost.toFixed(2)} Flasks🧪: Get ${availableItems[i].name}<br>${availableItems[i].description}<br>Increases Auto Generation by ${availableItems[i].rate}<br>Current Level: ${availableItems[i].upCount}`;
   app.append(buttons[i]);
   buttons[i].disabled = true;
 
   buttons[i].onclick = () => {
-    if (click_count >= availableItems[i].cost) {
-      click_count -= availableItems[i].cost;
-      availableItems[i].cost *= 1.15;
-      auto_growth += availableItems[i].rate;
-      auto_growth_report.innerHTML = `Current Auto Generation Level: ${auto_growth.toFixed(1)} Flasks/sec`;
-      availableItems[i].up_count++;
+    if (clickCount >= availableItems[i].cost) {
+      clickCount -= availableItems[i].cost;
+      availableItems[i].cost *= buttonCostScalar;
+      autoGrowth += availableItems[i].rate;
+      autoGrowthReport.innerHTML = `Current Auto Generation Level: ${autoGrowth.toFixed(1)} Flasks/sec`;
+      availableItems[i].upCount++;
       buttons[i].innerHTML =
-        `${availableItems[i].cost.toFixed(2)} Flasks🧪: Hire ${availableItems[i].name}<br>Increases Auto Generation by ${availableItems[i].rate}<br>Current Level: ${availableItems[i].up_count}`;
+        `${availableItems[i].cost.toFixed(2)} Flasks🧪: Hire ${availableItems[i].name}<br>Increases Auto Generation by ${availableItems[i].rate}<br>Current Level: ${availableItems[i].upCount}`;
     }
   };
 }
 
-const auto_growth_report = document.createElement("div");
-auto_growth_report.innerHTML = `Current Auto Generation Level: ${auto_growth} Flasks/sec`;
-app.append(auto_growth_report);
+const autoGrowthReport = document.createElement("div");
+autoGrowthReport.innerHTML = `Current Auto Generation Level: ${autoGrowth} Flasks/sec`;
+app.append(autoGrowthReport);
 
-let previous_time = performance.now();
-let current_time = 0;
+let previousTime = performance.now();
+let currentTime = 0;
 const autoIncrement = function () {
-  current_time = performance.now();
-  incrementCounter(auto_growth * ((current_time - previous_time) / 1000));
-  previous_time = current_time;
+  currentTime = performance.now();
+  incrementCounter(autoGrowth * ((currentTime - previousTime) / 1000));
+  previousTime = currentTime;
 
+  decideButtonEnable();
+
+  requestAnimationFrame(autoIncrement);
+};
+
+const decideButtonEnable  = function() {
   for (let i = 0; i < buttons.length; i++) {
-    if (click_count >= availableItems[i].cost) {
+    if (clickCount >= availableItems[i].cost) {
       buttons[i].disabled = false;
     } else {
       buttons[i].disabled = true;
     }
   }
-  requestAnimationFrame(autoIncrement);
 };
 
 requestAnimationFrame(autoIncrement);
